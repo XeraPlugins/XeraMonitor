be30929a2dd76dc54883abb19b807c2a6a80a5b3
Leeeunderscore <43287584+Leeeunderscore@users.noreply.github.com>
2020-06-17 21:14:27 -0600

    1.0.0
    Example: https://tps.6b6t.org is where it shows my tps so sites like http://tps.winsvideo.net/ can use it to compare it to other servers
    feel free to fork but please include credit

---
+++ LeeesTpsMonitor/src/main/java/Leees/Chat/Whitelist/Main.java
@@ -0,0 +1,115 @@
+package Leees.Chat.Whitelist;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    HashMap<String , Player > playerMap = new HashMap<String , Player>();
+    public ArrayList<String> playerlist;
+
+    public static File indexhtml;
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        this.playerlist = new ArrayList<String>();
+        File dir = new File("plugins/LeeesTpsMonitor");
+        try {
+            ReCaptcha.main();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        indexhtml = new File(dir, "index.html");
+        if (!indexhtml.exists()) {
+            try {
+                indexhtml.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                getServer().getPluginManager().disablePlugin(this);
+                return;
+            }
+            if (indexhtml.exists()) {
+                try {
+                    String fileContent = "<html>\n" +
+                            "  <head>\n" +
+                            "    <title>LeeesTpsMonitor</title>\n" +
+                            "    <meta http-equiv=\"refresh\" content=\"5\" >\n" +
+                            "  </head>\n" +
+                            "  <body>\n" +
+                            "    <form action=\"/submit\">\n" +
+                            "      <div  align=\"center\"><label for=\"username\">\n" +
+                            "    <br><br>\n" +
+                            "    <img src=\"https://cdn.discordapp.com/attachments/703638060792938578/710023538840698880/server-icon.png\"  width=\"64\" height=\"64\">\n" +
+                            "    <b><h1>LeeesTpsMonitor</h1></b></label>\n" +
+                            "    <h1>TPS:</h1> <h1 style=\"color:#00ff00;\">server_tps</h1>\n" +
+                            "    <br>\n" +
+                            "    <h1>PlayerCount:<h1> <h1 style=\"color:#00ff00;\">server_playercount</h1>\n" +
+                            "    <br>\n" +
+                            "    <h1>PlayerList:</h1>\n" +
+                            "    <h4 style=\"color:#00ff00;\">server_playerlist</h4>\n" +
+                            "      </div>  </body>\n" +
+                            "          <style>\n" +
+                            "body {\n" +
+                            "    color: black;\n" +
+                            "    background-image:url('https://cdn.discordapp.com/attachments/699025897373827073/714543738118209566/2020-05-25_11.21.34.png');\n" +
+                            "    background-repeat: no-repeat;\n" +
+                            "    background-size: 100% 100%;\n" +
+                            "    font-family: Verdana,Geneva,sans-serif; \n" +
+                            "}\n" +
+                            "h1 {\n" +
+                            "  color: white;\n" +
+                            "}\n" +
+                            "h2 {\n" +
+                            "  color: white;\n" +
+                            "}\n" +
+                            "h3 {\n" +
+                            "  color: white;\n" +
+                            "}\n" +
+                            "h4 {\n" +
+                            "  color: white;\n" +
+                            "}\n" +
+                            "h5 {\n" +
+                            "  color: white;\n" +
+                            "}\n" +
+                            "h6 {\n" +
+                            "  color: white;\n" +
+                            "}\n" +
+                            "html {\n" +
+                            "    height: 100%\n" +
+                            "}\n" +
+                            ".class { \n" +
+                            "  font-family: Verdana,Geneva,sans-serif; \n" +
+                            "}\n" +
+                            "            body, html\n" +
+                            "            {\n" +
+                            "                margin: 0; padding: 0; height: 100%; overflow: hidden;\n" +
+                            "            }\n" +
+                            "\n" +
+                            "            #content\n" +
+                            "            {\n" +
+                            "                position:absolute; left: 0; right: 0; bottom: 0; top: 0px; \n" +
+                            "            }\n" +
+                            "</style>\n" +
+                            "</html>";
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("plugins/LeeesTpsMonitor/index.html"));
+                    writer.write(fileContent);
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    getServer().getPluginManager().disablePlugin(this);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ LeeesTpsMonitor/src/main/java/Leees/Chat/Whitelist/ReCaptcha.java
@@ -0,0 +1,51 @@
+package Leees.Chat.Whitelist;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.bukkit.Bukkit;
+import net.minecraft.server.v1_12_R1.MinecraftServer;
+import java.io.*;
+import java.net.InetSocketAddress;
+public class ReCaptcha {
+
+    public static void main() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(6767),0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+    public static class MyHandler
+            implements HttpHandler {
+        public void handle(HttpExchange t) {
+            StringBuilder contentBuilder = new StringBuilder();
+            try {
+                BufferedReader in = new BufferedReader(new FileReader("plugins/LeeesTpsMonitor/index.html"));
+                String str;
+                while ((str = in.readLine()) != null) {
+                    contentBuilder.append(str);
+                }
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String response = contentBuilder.toString().replaceAll("server_tps", String.valueOf(MinecraftServer.getServer().recentTps[0])).replaceAll("server_playercount", String.valueOf(Bukkit.getServer().getOnlinePlayers().size())).replaceAll("server_playerlist", String.valueOf(PlayerGeter.getPlayers()));
+            try {
+                t.sendResponseHeaders(200, response.length());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            OutputStream os = t.getResponseBody();
+            try {
+                os.write(response.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
